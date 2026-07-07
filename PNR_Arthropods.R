ff16ae3b8544cf141dcaaa5322e2a770ee304ce8
manoj-pandey260 <pandey.243@osu.edu>
2026-07-07 12:57:46 -0400

    data update

--- PNR_Arthropods.R
+++ PNR_Arthropods.R
@@ -21,9 +21,11 @@ colSums(is.na(pnr15))
 pnr15 <- pnr15 %>%  mutate(across(6:last_col(), ~ as.numeric(as.character(.))))
 
 
-# Combining Dicyrtomidae and Katiannidae as Globular Springtails
-pnr15 <- pnr15 %>% 
-  mutate(Glob_Springt = Dicyrtomidae + Katiannidae, .keep = "unused")
+# # Combining Dicyrtomidae and Katiannidae as Globular Springtails
+# pnr15 <- pnr15 %>% 
+#   mutate(Glob_Springt = Dicyrtomidae + Katiannidae, .keep = "unused")
+
+pnr15 <- pnr15[, !(names(pnr15) %in% c("Dicyrtomidae", "Katiannidae"))]
 
 # ---- 2022 data ----
 pnr2022 <- read.csv("Data/Processed/Powdermill_Community_2022.csv")
@@ -42,11 +44,13 @@ pnr22 <- pnr22 %>% mutate(Treatment = dplyr::recode(Treatment,
                                                     "windthrow" = "Windthrow"))
 levels(pnr22$Treatment)
 
-pnr22 <- pnr22 %>% select(-Coleoptera, -Collembola, -Protura, -Diplura)
+pnr22 <- pnr22 %>% dplyr::select(-Coleoptera, -Collembola, -Protura, -Diplura)
 
 # Combining Dicyrtomidae, Katiannidae, Bourletiellidae and Sminthuridae as Globular Springtails
-pnr22 <- pnr22 %>% 
-  mutate(Glob_Springt = Dicyrtomidae + Katiannidae + Bourletiellidae + Sminthuridae, .keep = "unused")
+# pnr22 <- pnr22 %>% 
+#   mutate(Glob_Springt = Dicyrtomidae + Katiannidae + Bourletiellidae + Sminthuridae, .keep = "unused")
+
+pnr22 <- pnr22[, !(names(pnr22) %in% c("Dicyrtomidae", "Katiannidae", "Bourletiellidae" , "Sminthuridae"))]
 
 pnr22 <- pnr22 %>% rename(Ptiliidae = Ptilidae)
 
@@ -65,27 +69,30 @@ pnr22 <- pnr22 %>% rename(Ptiliidae = Ptilidae)
 pnr15$Year <- "2015"
 pnr22$Year <- "2022"
 
-species_cols_pnr15 <- colnames(pnr15)[!(colnames(pnr15) %in% c("Quadrat", "Treatment", "Interval", "DateSet" , "DateColl", "Year"))]
-species_cols_pnr22 <- colnames(pnr22)[!(colnames(pnr22) %in% c("Quadrat", "Treatment", "Interval", "DateSet" , "DateColl", "Year"))]
+# species_cols_pnr15 <- colnames(pnr15)[!(colnames(pnr15) %in% c("Quadrat", "Treatment", "Interval", "DateSet" , "DateColl", "Year"))]
+# species_cols_pnr22 <- colnames(pnr22)[!(colnames(pnr22) %in% c("Quadrat", "Treatment", "Interval", "DateSet" , "DateColl", "Year"))]
+# 
+# 
+# all_species <- union(species_cols_pnr15, species_cols_pnr22)  # all species in both years combined
+# 
+# 
+# 
+# missing_in_pnr15 <- setdiff(all_species, species_cols_pnr15)
+# pnr15[missing_in_pnr15] <- 0
+# 
+# 
+# missing_in_pnr22 <- setdiff(all_species, species_cols_pnr22)
+# # pnr22[missing_in_pnr22] <- 0
+# 
+# 
+# 
+# pnr15 <- pnr15[, c("Quadrat", "Treatment", "Interval", "DateSet" , "DateColl", "Year", all_species)]
+# pnr22 <- pnr22[, c("Quadrat", "Treatment", "Interval", "DateSet" , "DateColl", "Year", all_species)]
 
+pnr22$Odontellidae <- NULL
 
-all_species <- union(species_cols_pnr15, species_cols_pnr22)  # all species in both years combined
-
-
-
-missing_in_pnr15 <- setdiff(all_species, species_cols_pnr15)
-pnr15[missing_in_pnr15] <- 0
-
-
-missing_in_pnr22 <- setdiff(all_species, species_cols_pnr22)
-# pnr22[missing_in_pnr22] <- 0
-
-
-
-pnr15 <- pnr15[, c("Quadrat", "Treatment", "Interval", "DateSet" , "DateColl", "Year", all_species)]
-pnr22 <- pnr22[, c("Quadrat", "Treatment", "Interval", "DateSet" , "DateColl", "Year", all_species)]
-
-
+colnames(pnr15)
+colnames(pnr22)
 
 all_data <- rbind(pnr15, pnr22)
 
@@ -103,7 +110,8 @@ traploc <- traploc[ , c(1,4)]
 all_data <- left_join(all_data, traploc, by = "Quadrat")
 
 
-all_data <- all_data %>%  relocate(53, .before = 3)
+all_data <- all_data %>%  relocate(51, .before = 3)
+all_data <- all_data %>%  relocate(51, .before = 3)
 
 
 colSums(is.na(all_data))
@@ -114,9 +122,9 @@ all_data <- all_data %>%  mutate(across(8:last_col(), ~ as.numeric(as.character(
 
 library(vegan)
 
-all_data$Abundance <- rowSums(all_data[,8:53], na.rm=TRUE)
-all_data$Richness <- apply(all_data[,8:53]>0,1,sum)
-all_data$Diversity <- all_data %>% select(-c(1:7)) %>%  diversity(index = "shannon")
+all_data$Abundance <- rowSums(all_data[,8:51], na.rm=TRUE)
+all_data$Richness <- apply(all_data[,8:51]>0,1,sum)
+all_data$Diversity <- all_data %>% dplyr::select(-c(1:7,52,53)) %>%  diversity(index = "shannon")
 str(all_data)
 all_data$Richness <- as.numeric(all_data$Richness)
 
@@ -127,7 +135,7 @@ all_data$DateSet  <- as.Date(all_data$DateSet, format = "%m/%d/%Y")
 
 all_data$TrapTime <- as.numeric(all_data$DateColl - all_data$DateSet)
 
-all_data <- all_data %>%  relocate(57, .before = 7)
+all_data <- all_data %>%  relocate(55, .before = 8)
 
 
 write.csv(all_data, "Data/Saved/pnr_clean.csv", row.names = FALSE)
@@ -174,9 +182,12 @@ summary(model1)
 
 
 library(emmeans)
+library(multcomp)
 
+model1l <- emmeans(model1, ~ Treatment * Year)
+cld(model1l, Letters = letters)
+emmip(model1, Treatment ~ Year, type = "response")
 
-emmeans(model1, ~ Treatment * Year)
 
 mean(all_data$Richness)
 var(all_data$Richness)
@@ -186,7 +197,9 @@ model2 <- glmmTMB(Richness ~ Treatment * Year + (1|Transect)+ offset(log(TrapTim
 
 summary(model2)
 
-emmeans(model2, ~ Treatment * Year)
+model2l <- emmeans(model2, ~ Treatment * Year)
+cld(model2l, Letters = letters)
+emmip(model2, Treatment ~ Year, type = "response")
 
 
 
@@ -198,20 +211,20 @@ hist(all_data$Diversity)
 model3 <- glmmTMB(Diversity ~ Treatment * Year + (1|Transect)+ offset(TrapTime), family=gaussian(), data = all_data)
 summary(model3)
 
-emmeans(model3, ~ Treatment * Year)
-emmeans(model3, ~ Year)
-
+model3l <- emmeans(model3, ~ Treatment * Year)
+cld(model3l, Letters = letters)
+emmip(model3, Treatment ~ Year)
 
 # ---- NMDS ----
 
 library(vegan)
- community <- all_data[, -c(1:8, 55:57)]
- meta <- all_data[, c(2,8)]
+ community <- all_data[, -c(1:8, 53:55)]
+ meta <- all_data[, c(2,3)]
 
  
  nmds1 <- metaMDS(community, 
                 distance = "bray",   
-                k = 2,               
+                k = 3,               
                 trymax = 100)
  
  
@@ -228,13 +241,13 @@ library(vegan)
  
  all_2015 <- all_data[which(all_data$Year== "2015"), ]
  
- community15 <- all_2015[, -c(1:8, 55:57)]
+ community15 <- all_2015[, -c(1:8, 53:55)]
  meta15 <- all_2015[, c(1,2)]
  
  
  nmds15 <- metaMDS(community15, 
                   distance = "bray",   
-                  k = 2,               
+                  k = 3,               
                   trymax = 100)
  
  
@@ -250,6 +263,13 @@ library(vegan)
    labs(title = "PNR 2015") +
    theme(plot.title = element_text(hjust = 0.5))
  
+ ggplot(scores_15, aes(NMDS1, NMDS3, color = Treatment, fill = Treatment)) +
+   geom_point(size = 3) +
+   stat_ellipse(geom = "polygon", alpha = 0.2, color = NA)+
+   theme_minimal() +
+   labs(title = "PNR 2015") +
+   theme(plot.title = element_text(hjust = 0.5))
+ 
  adonis15 <- adonis2(community15 ~ Treatment, data = meta15, method = "bray")
  adonis15
 
@@ -257,13 +277,13 @@ library(vegan)
  
  all_2022 <- all_data[which(all_data$Year== "2022"), ]
  
- community22 <- all_2022[, -c(1:8, 55:57)]
+ community22 <- all_2022[, -c(1:8, 53:55)]
  meta22 <- all_2022[, c(1,2)]
  
  
  nmds22 <- metaMDS(community22, 
                    distance = "bray",   
-                   k = 2,               
+                   k = 3,               
                    trymax = 100)
  
  
@@ -278,6 +298,16 @@ library(vegan)
    theme_minimal() +
    labs(title = "PNR 2022") +
    theme(plot.title = element_text(hjust = 0.5))
+ 
+ ggplot(scores_22, aes(NMDS1, NMDS3, color = Treatment, fill = Treatment)) +
+   geom_point(size = 3) +
+   stat_ellipse(geom = "polygon", alpha = 0.2, color = NA)+
+   theme_minimal() +
+   labs(title = "PNR 2022") +
+   theme(plot.title = element_text(hjust = 0.5))
+ 
+ 
+ 
  
  
  adonis22 <- adonis2(community22 ~ Treatment, data = meta22, method = "bray")
@@ -322,7 +352,7 @@ library(vegan)
 # ---- Species summaries ----
  colnames(all_data)
  library(dplyr)
- library(multcomp)
+
  species_summary <- all_data %>%
    dplyr::select(-Quadrat, -Treatment, -Transect, -Interval, -DateSet, -DateColl, -TrapTime, -Year, -Abundance, -Richness, -Diversity) %>%
    dplyr::summarise(across(everything(), ~ mean(. > 0, na.rm = TRUE))) %>%
@@ -341,112 +371,107 @@ library(vegan)
  summary(aran)
  simulateResiduals(aran, plot = TRUE)
  aranl <- emmeans(aran, ~ Treatment * Year)
- cld(aranl, Letters = letters, adjust = "tukey")
+ cld(aranl, Letters = letters)
  emmip(aran, Treatment ~ Year)
  
  tomo <- glmmTMB(Tomoceridae ~ Treatment * Year + (1|Transect)+  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(tomo)
  tomol <- emmeans(tomo, ~ Treatment * Year)
- cld(tomol, Letters = letters, adjust = "tukey")
+ cld(tomol, Letters = letters)
  emmip(tomo, Treatment ~ Year)
  
  form <- glmmTMB(Formicidae ~ Treatment * Year + (1|Transect)+  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(form)
  forml <- emmeans(form, ~ Treatment * Year)
- cld(forml, Letters = letters, adjust = "tukey")
+ cld(forml, Letters = letters)
  emmip(form, Treatment ~ Year)
  
 
  symp <- glmmTMB(Symphypleona ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(symp)
- sympl <- emmeans(symp, ~ Treatment * Year)
- cld(sympl, Letters = letters, adjust = "tukey")
+ sympl <- emmeans(symp, ~ Treatment | Year)
+ cld(sympl, Letters = letters)
  emmip(symp, Treatment ~ Year)
  
- glob <- glmmTMB(Glob_Springt ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
- summary(glob)
- globl <- emmeans(glob, ~ Treatment * Year)
- cld(globl, Letters = letters, adjust = "tukey")
- emmip(glob, Treatment ~ Year)
  
  hypo <- glmmTMB(Hypogastruridae ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(hypo)
  hypol <- emmeans(hypo, ~ Treatment * Year)
- cld(hypol, Letters = letters, adjust = "tukey")
+ cld(hypol, Letters = letters)
  emmip(hypo, Treatment ~ Year)
  
  
  stap <- glmmTMB(Staphylinidae ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(stap)
  stapl<-emmeans(stap, ~ Treatment * Year)
- cld(stapl, Letters = letters, adjust = "tukey")
+ cld(stapl, Letters = letters)
  emmip(stap, Treatment ~ Year)
  
  
  isot <- glmmTMB(Isotomidae ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(isot)
  isotl<-emmeans(isot, ~ Treatment * Year)
- cld(isotl, Letters = letters, adjust = "tukey")
+ cld(isotl, Letters = letters)
  emmip(isot, Treatment ~ Year)
  
  
  poly <- glmmTMB(Polydesmida ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(poly)
- polyl<-emmeans(poly, ~ Treatment * Year)
- cld(polyl, Letters = letters, adjust = "tukey")
+ polyl<-emmeans(poly, ~ Treatment|Year)
+ cld(polyl, Letters = letters)
  emmip(poly, Treatment ~ Year)
  
  ento <- glmmTMB(Entomobryidae ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(ento)
  entol<-emmeans(ento, ~ Treatment * Year)
- cld(entol, Letters = letters, adjust = "tukey")
+ cld(entol, Letters = letters)
  emmip(ento, Treatment ~ Year)
  
  cara <- glmmTMB(Carabidae ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(cara)
- caral<-emmeans(cara, ~ Treatment * Year)
- cld(caral, Letters = letters, adjust = "tukey")
+ caral<-emmeans(cara, ~ Treatment | Year)
+ cld(caral, Letters = letters, adjust="tukey")
  emmip(cara, Treatment ~ Year)
 
  snai <- glmmTMB(Snails ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(snai)
  snail<-emmeans(snai, ~ Treatment * Year)
- cld(snail, Letters = letters, adjust = "tukey")
+ cld(snail, Letters = letters)
  emmip(snai, Treatment ~ Year)
  
  
  nean <- glmmTMB(Neanuridae ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(nean)
  neanl<-emmeans(nean, ~ Treatment * Year)
- cld(neanl, Letters = letters, adjust = "tukey")
+ cld(neanl, Letters = letters)
  emmip(nean, Treatment ~ Year)
  
  
  opil <- glmmTMB(Opiliones ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(opil)
  opill<-emmeans(opil, ~ Treatment * Year)
- cld(opill, Letters = letters, adjust = "tukey")
+ cld(opill, Letters = letters)
  emmip(opil, Treatment ~ Year)
  
  
  niti <- glmmTMB(Nitidulidae ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(niti)
  nitil<-emmeans(niti, ~ Treatment * Year)
- cld(nitil, Letters = letters, adjust = "tukey")
+ cld(nitil, Letters = letters)
  emmip(niti, Treatment ~ Year)
  
  
  scol <- glmmTMB(Scolytinae ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(scol)
- scoll<-emmeans(scol, ~ Treatment * Year)
- cld(scoll, Letters = letters, adjust = "tukey")
+ scoll<-emmeans(scol, ~ Treatment | Year)
+ cld(scoll, Letters = letters, adjust="tukey")
  emmip(scol, Treatment ~ Year)
  
  
  rhap <- glmmTMB(Rhaphidophoridae ~ Treatment * Year +  offset(log(TrapTime)), family=nbinom2(), data = all_data)
  summary(rhap)
  rhapl<-emmeans(rhap, ~ Treatment * Year)
- cld(rhapl, Letters = letters, adjust = "tukey")
+ cld(rhapl, Letters = letters)
  emmip(rhap, Treatment ~ Year)
  
  
